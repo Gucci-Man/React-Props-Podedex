1858560c394731c9a7ac0e782e5ba4702407c53e
mrtomno <adel.ngo@yahoo.com>
2024-01-14 13:29:59 -0600

    pass data to pokecard

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Pokecard = (props) => {
+const Pokecard = ({id, name, type, base_experience}) => {
+    const img_src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
     return (
         <div>
-            <h1>Pokecard goes here</h1>
+            <h3>{name}</h3>
+            <img src = {img_src} />
+            <p>Type: {type}</p>
+            <p>EXP: {base_experience}</p>
         </div>
     );
 };
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Pokecard from './Pokecard';
 
-const Pokedex = (pokemon) => {
+
+
+const Pokedex = ({pokemon}) => {
+    console.log(pokemon[0])
     return (
         <div>
             <h1>Pokecards go here</h1>
-            <Pokecard />
+            {<Pokecard key={pokemon[0].id} id={pokemon[0].id} name={pokemon[0].name} type={pokemon[0].type} base_experience={pokemon[0].base_experience} />}
         </div>
     )
 }
 
-Pokedex.defaultProps = {
+/* Pokedex.defaultProps = {
     pokemon: [
         {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
         {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
@@ -22,5 +25,5 @@ Pokedex.defaultProps = {
         {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
       ],
 };
-
+ */
 export default Pokedex;
